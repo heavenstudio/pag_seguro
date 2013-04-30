eb303f65939106224a48363bf9bcb30f322497d1
Ewerton Miglioranza <efmiglioranza@gmail.com>
2013-04-30 02:05:14 -0300

    Created methods to query transaction history and abandoned transactions. Remove inheritance from Query. Added class methods instead, as the class doesn't hold any data.

--- spec/pag_seguro/query_spec.rb
+++ spec/pag_seguro/query_spec.rb
@@ -3,9 +3,17 @@
 require 'spec_helper'
 
 describe PagSeguro::Query do
-  before { PagSeguro::Query.any_instance.stub(transaction_data: transaction_data) }
-  let(:transaction){ PagSeguro::Query.new("mail", "token", "trans_code") }
-  subject{ PagSeguro::Query }
+  describe :query_transaction do
+    before{ RestClient.stub(get: transaction_xml) }
+    let(:transaction){ PagSeguro::Query.query_transaction("mail", "token", "trans_code") }
+    subject{ PagSeguro::Query.query_transaction("mail", "token", "trans_code").class }
 
-  it_behaves_like "a transaction"
+    it_behaves_like "a transaction"
+  end
+
+  describe :query_transaction_history do
+  end
+
+  describe :query_abandoned_transactions do
+  end
 end
