849c43b9f9c55204b1a0b55c5784f6ff88dd64e7
Ewerton Miglioranza <efmiglioranza@gmail.com>
2013-05-05 17:09:55 -0300

    Fixed decimal validator regexp with proper string delimiters. More info: http://www.ruby-doc.org/docs/ProgrammingRuby/html/language.html#UL

--- lib/pagseguro_decimal_validator.rb
+++ lib/pagseguro_decimal_validator.rb
@@ -1,6 +1,6 @@
 class PagseguroDecimalValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value.nil? || value =~ /^\d+\.\d{2}$/
+    unless value.nil? || value =~ /\A\d+\.\d{2}\z/
       object.errors.add(attribute, error_message)
     end
   end
